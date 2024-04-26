ec43f63466ddfa72caae1b264be3689dce5bee1b
imen <yousriimen09@gmail.com>
2024-04-26 15:58:52 +0200

    Front end

--- src/components/TwoLevelPieChart_Card/TowLevelPieChart_Card.jsx
+++ src/components/TwoLevelPieChart_Card/TowLevelPieChart_Card.jsx
@@ -14,12 +14,17 @@ const TowLevelPieChart_Card = (props) => {
     }
     return (
         <>
-            <div className="inline-block w-[40%] h-[40%]">
+            <div className="inline-block w-[40%] h-[40%] sm:h-[30%]">
                 <div className='bg-[--card-color] text-[--light-color] m-6 ml-6 cursor-pointer  h-[60%] w-[90%] font-Main-font transition duration-700 hover:border-2 border-inset border-royal-blue shadow-lg' onClick={click}>
-                    <p className='container text-3xl mb-3'>{props.title}</p>
+                    <p className='container text-3xl mb-3
+                                    sm:text-xl'>
+                        {props.title}
+                    </p>
                     <button className=' text-2xl bg-Menu-hover border-b border-solid border-light-color border-r'>
-                        <a href="" className="text-light-color">More Info</a>
-                        <MdReadMore  className='ml-1'/>
+                        <a href="" className="sm:text-sm">
+                            More Info
+                        </a>
+                        <MdReadMore  className='sm:text-sm'/>
                     </button>
                 </div>
 
